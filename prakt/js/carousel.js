be210bc0016eeec1b51227b73c3573d1fe1cfc2f
Thairon922 <51987798+Thairon922@users.noreply.github.com>
2019-06-19 10:24:30 +0300

    Add files via upload

---
+++ prakt/js/carousel.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+      items:4.1,loop:true,center:true,dots:false
+
+  });
+});
---
+++ prakt/js/search.js
@@ -0,0 +1,11 @@
+$(document).mouseup(function (e){
+                        var div = $(".search_block");
+                        if (!div.is(e.target) 
+                            && div.has(e.target).length === 0) {
+                            div.hide();
+                        }
+                    });
+                    
+                    $('.search').click(function(){
+                        $('.search_block').show();
+                    });

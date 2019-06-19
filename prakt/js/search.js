dca5f7d2ecbc9008eb907d36c3eaba560eee557b
Thairon922 <51987798+Thairon922@users.noreply.github.com>
2019-06-19 10:26:28 +0300

    Update search.js

--- prakt/js/search.js
+++ prakt/js/search.js
@@ -1,11 +1,11 @@
 $(document).mouseup(function (e){
-                        var div = $(".search_block");
-                        if (!div.is(e.target) 
-                            && div.has(e.target).length === 0) {
-                            div.hide();
-                        }
-                    });
-                    
-                    $('.search').click(function(){
-                        $('.search_block').show();
-                    });
+var div = $(".search_block");
+if (!div.is(e.target) 
+  && div.has(e.target).length === 0) {
+  div.hide();
+  }
+});
+
+$('.search').click(function(){
+  $('.search_block').show();
+});
